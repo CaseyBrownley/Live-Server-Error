a225489ff82023919eef95ad6c749d9fb43b317e
CaseyBrownley <162181390+CaseyBrownley@users.noreply.github.com>
2024-03-20 13:05:14 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,108 @@
+//Operands/Primitives Unit
+// alert("It's Working");
+// console.log(3+4);
+// // console.log connects js file to console file in browser//
+// console.error('Oh Nooo');
+// console.log(3+4, 'Hello', true);
+
+// //Conditionals examples//
+// if(1===1){
+//     console.log("It's True");
+// }
+// if(rating===3){
+//     console.log(`You Are a Superstar!`)
+// }
+// let num = 37
+// if (num % 2 !== 0){
+// console.log(`Odd Number`)
+// }
+// let (rating=2);
+// if (rating===2){
+//     console.log(`Meets Expectations`)
+// }
+// if (rating===3){
+//     console.log(`You Are a Superstar`)
+// }
+// else if(rating===1){
+//     console.log(`Needs Improvement`)
+// }
+// else{
+//     console.log(Invalid Rating)
+// }
+// let password=`HelloKitty`;
+// if(password.length>=6){
+// if(password.indexOf(``)===-1){
+//     console.log("Valid Password");
+// }
+// else{
+//     console.log(`Password is long enough, but cannot contain spaces`);
+// }
+// }
+// else{
+//     console.log("Password Must be Longer")
+// }
+// let mystery = 5;
+// if(mystery){
+//     console.log(`truthy`);
+// }
+// else{
+//     console.log(`falsy`);
+// }
+
+// true && true
+
+// if(password.length>= 8 && password.indexOf(` `)===-1){
+//     console.log(Valid Password);
+// }
+// else{
+//     console.log(`Invalid Password`);
+// }
+// let age=78;
+// if (age < 6 || age >= 65){
+//     console.log(`You Get In For Free`);
+// }
+// else{
+//     console.log(`You Must Pay`);
+// }
+// !null
+// let day-2
+// switch(day){
+//     case 1:
+//         console.log(`Monday`);
+//         break;
+//     case 2:
+//         console.log(`Tuesday`);
+//         break;``;
+// }
+// let num=7;
+// if (num === 7){
+// console.log(`Lucky`)
+// }
+// else {
+//     console.log(`Bad`);
+// }
+// num=7 ? console.log(`Lucky`) : console.log(`Bad`);
+
+//ARRAYS UNIT
+// let shoppingList= [`cereal`, `ice cream`, `cheese`];
+// let lotto= [1,2,3,4,5,6];
+// shoppingList[0]= `Special K`;
+// //this overrides what is in let
+// shoppingList[1]= `Ben and Jerrys`;
+// shoppingList[shoppingList.length]=`Tomatoes`;
+// shoppingList[shoppingList.length]=`Fruit`;
+//Array
+// let topSongs = [
+//     `Fetti`, `Uber Everywhere`;
+// ]
+// topSongs.push(`Fortunate Sons`);
+// let dishesToDO = [`plate`]
+// dishesToDO.unshift(bowl)
+
+// let fruits = [`apple,` `banana`]
+// let veggies = [`brussels,` `eggplant`]
+// console.log(fruits.concat(veggies));
+// let allFood = fruits.concat(veggies, meat);
+
+let animals = [`shark,` `dolphin,` `fish,`];
+let swimmers = animals.slice(0,2);
